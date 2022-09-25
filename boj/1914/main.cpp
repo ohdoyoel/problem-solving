56a1cece19c5f424594d10e78f0008284ac20521
Daniel Oh <ohdoyoel@naver.com>
2022-09-25 12:51:03 +0900

    alm 0927

---
+++ boj/1914/main.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+using namespace std;
+using lld = long long;
+using pii = pair<int, int>;
+using pll = pair<lld, lld>;
+
+// variable
+///////////////////////////////////////
+uint n;
+
+unsigned lld inst_len(uint k)
+{
+    if (k == 1) return 1;
+    return 2 * inst_len(k - 1) + 1;
+}
+
+uint other(uint x, uint y)
+{
+    if ((x == 1 && y == 2) || (x == 2 && y == 1))
+        return 3;
+    else if ((x == 1 && y == 3) || (x == 3 && y == 1))
+        return 2;
+    else if ((x == 2 && y == 3) || (x == 3 && y == 2))
+        return 1;
+}
+
+void solve(uint k, uint current, uint target)
+{
+    if (k == 1)
+    {
+        cout << current << ' ' << target << endl;
+        return;
+    }
+    solve(k - 1, current, other(current, target));
+    solve(1, current, target);
+    solve(k - 1, other(current, target), target);
+    return;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+    // input
+    ///////////////////////////////////////
+    cin >> n;
+
+    // algorithm
+    ///////////////////////////////////////
+    if (n <= 20)
+    {
+        cout << inst_len(n) << endl;
+        solve(n, 1, 3);
+    }
+    else
+    {
+        cout << inst_len(n) << endl;
+    }
+    
+    // output
+    ///////////////////////////////////////
+
+    return 0;
+}
+
+// pseudo code
+///////////////////////////////////////
+/*
+uint n <- input
+table <- [vector<int>a(0~n), vector<int>b, vector<int>c]
+
+void solve(n, current, target)
+    if (n == 1)
+        table[current].back to table[target].push_back
+        print(current, target)
+        return ;
+    solve(n - 1, current, space)
+    print(1, current, target)
+    return ;
+
+solve(n, 1, 3)
+*/
