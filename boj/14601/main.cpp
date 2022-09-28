7700903102f8c5a929405f26f58fc27b26a7a0ab
Daniel Oh <ohdoyoel@naver.com>
2022-09-28 11:22:53 +0900

    alm 0927

--- boj/14601/main.cpp
+++ boj/14601/main.cpp
@@ -14,53 +14,50 @@ uint x, y;
 
 // table에서 2^k 만큼의 정사각형 범위을 (x0, y0)에서 부터 탐색할 것, (x, y)는 배수구의 상대 위치
 uint e = 1;
-bool flag = true;
-void solve(vector<vector<int>> &table, uint n, uint x0, uint y0, uint x, uint y)
+void solve(vector<vector<int>> &table, uint n, uint x0, uint y0, int _x, int _y)
 {    
-    if (e > 5500)
-    {
-        flag = false;
-        return;
-    }
-    uint p_n = pow(2, n);
     if (n == 1)
     {
-        for (uint r = x0; r < x0 + p_n; r++)
+        for (uint r = x0; r < x0 + 2; r++)
         {
-            for (uint c = y0; c < y0 + p_n; c++)
+            for (uint c = y0; c < y0 + 2; c++)
             {
-                if (r != (x0 + x) || c != (y0 + y)) table[r][c] = e;
+                if (r != (x0 + _x) || c != (y0 + _y))
+                {
+                    table[r][c] = e;
+                    //cout << e << " r:" << r << " c:" << c << " x0,y0" << x0 << "," << y0 << " x,y" << _x << "," << _y << endl;
+                }
             }
         }
         e++;
         return;
     }
     uint p_n_minus_one = pow(2, n - 1);
-    if (x < p_n_minus_one && y < p_n_minus_one)
+    if (_x < p_n_minus_one && _y < p_n_minus_one)
     {
         table[x0 + p_n_minus_one][y0 + p_n_minus_one - 1] = e;
         table[x0 + p_n_minus_one - 1][y0 + p_n_minus_one] = e;
         table[x0 + p_n_minus_one][y0 + p_n_minus_one] = e;
         e++;
-        solve(table, n - 1, x0, y0, x - x0, y - y0);
+        solve(table, n - 1, x0, y0, _x, _y);
         solve(table, n - 1, x0 + p_n_minus_one, y0, 0, p_n_minus_one - 1);
         solve(table, n - 1, x0, y0 + p_n_minus_one, p_n_minus_one - 1, 0);
         solve(table, n - 1, x0 + p_n_minus_one, y0 + p_n_minus_one, 0, 0);
         return;
     }
-    else if (x >= p_n_minus_one && y < p_n_minus_one)
+    else if (_x >= p_n_minus_one && _y < p_n_minus_one)
     {
         table[x0 + p_n_minus_one - 1][y0 + p_n_minus_one - 1] = e;
         table[x0 + p_n_minus_one - 1][y0 + p_n_minus_one] = e;
         table[x0 + p_n_minus_one][y0 + p_n_minus_one] = e;
         e++;
         solve(table, n - 1, x0, y0, p_n_minus_one - 1, p_n_minus_one - 1);
-        solve(table, n - 1, x0 + p_n_minus_one, y0, x - x0, y -y0);
+        solve(table, n - 1, x0 + p_n_minus_one, y0, _x - (p_n_minus_one), _y);
         solve(table, n - 1, x0, y0 + p_n_minus_one, p_n_minus_one - 1, 0);
         solve(table, n - 1, x0 + p_n_minus_one, y0 + p_n_minus_one, 0, 0);
         return;
     }
-    else if (x < p_n_minus_one && y >= p_n_minus_one)
+    else if (_x < p_n_minus_one && _y >= p_n_minus_one)
     {
         table[x0 + p_n_minus_one - 1][y0 + p_n_minus_one - 1] = e;
         table[x0 + p_n_minus_one][y0 + p_n_minus_one - 1] = e;
@@ -68,11 +65,11 @@ void solve(vector<vector<int>> &table, uint n, uint x0, uint y0, uint x, uint y)
         e++;
         solve(table, n - 1, x0, y0, p_n_minus_one - 1, p_n_minus_one - 1);
         solve(table, n - 1, x0 + p_n_minus_one, y0, 0, p_n_minus_one - 1);
-        solve(table, n - 1, x0, y0 + p_n_minus_one, x - x0, y - y0);
+        solve(table, n - 1, x0, y0 + p_n_minus_one, _x, _y - (p_n_minus_one));
         solve(table, n - 1, x0 + p_n_minus_one, y0 + p_n_minus_one, 0, 0);
         return;
     }
-    else if (x >= p_n_minus_one && y >= p_n_minus_one)
+    else if (_x >= p_n_minus_one && _y >= p_n_minus_one)
     {
         table[x0 + p_n_minus_one - 1][y0 + p_n_minus_one - 1] = e;
         table[x0 + p_n_minus_one][y0 + p_n_minus_one - 1] = e;
@@ -81,7 +78,7 @@ void solve(vector<vector<int>> &table, uint n, uint x0, uint y0, uint x, uint y)
         solve(table, n - 1, x0, y0, p_n_minus_one - 1, p_n_minus_one - 1);
         solve(table, n - 1, x0 + p_n_minus_one, y0, 0, p_n_minus_one - 1);
         solve(table, n - 1, x0, y0 + p_n_minus_one, p_n_minus_one - 1, 0);
-        solve(table, n - 1, x0 + p_n_minus_one, y0 + p_n_minus_one, x - x0, y - y0);
+        solve(table, n - 1, x0 + p_n_minus_one, y0 + p_n_minus_one, _x - (p_n_minus_one), _y - (p_n_minus_one));
         return;
     }
 }
@@ -102,28 +99,30 @@ int main()
 
     // algorithm
     ///////////////////////////////////////
-    // 입력이 x, y 순서로 들어오기에, y, x -> r, c를 넣기
-    solve(table, k, 0, 0, y, x);
-    table[y][x] = -1;
+    // 입력이 x, y 순서로 들어오기에, r, c <- l - y, x
+    solve(table, k, 0, 0, pow(2, k) - 1 - y, x);
+    table[pow(2, k) - 1 - y][x] = -1;
 
     // output
     ///////////////////////////////////////
-    if (flag)
+    
+    for (int i = 0; i< sz(table); i++)
     {
-        for (int i = sz(table) - 1; i >= 0; i--)
+        for (uint j = 0; j < sz(table[0]); j++)
         {
-            for (uint j = 0; j < sz(table[0]); j++)
-            {
-                cout << table[i][j] << ' ';
-            }
-            cout << endl;
+            cout << table[i][j] << ' ';
         }
+        cout << endl;
     }
-    else
-    {
-        cout << -1 << endl;
-    }
-
+    
+    // for (uint i = 0; i < sz(table); i++)
+    // {
+    //     for (uint j = 0; j < sz(table[0]); j++)
+    //     {
+    //         cout << table[i][j] << ' ';
+    //     }
+    //     cout << endl;
+    // }
 
     return 0;
 }
