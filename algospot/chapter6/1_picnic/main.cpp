138f6f9c30f5b51322e92c43be4daa1176db1b07
Daniel Oh <ohdoyoel@gmail.com>
2023-11-03 15:36:20 +0000

    c/c++ runner added

---
+++ algospot/chapter6/1_picnic/main.cpp
@@ -0,0 +1,25 @@
+#include "bits/stdc++.h"
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+using namespace std;
+using lld = long long;
+using pii = pair<int, int>;
+using pll = pair<lld, lld>;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    int c;
+    cin >> c;
+
+    for (int i = 0; i < c; ++i){
+        int n, m;
+        cin >> n >> m;
+        cout << n << " " << m << endl;
+    }
+
+    return 0;
+}
