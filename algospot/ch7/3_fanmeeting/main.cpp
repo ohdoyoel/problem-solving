d2b320c63f9bac709e65759f19ac191aa7a86cb6
Daniel Oh <ohdoyoel@gmail.com>
2023-12-23 00:18:49 +0900

    algospot fanmeeting

---
+++ algospot/ch7/3_fanmeeting/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (uint)(x).size()
+using namespace std;
+using lld = long long;
+using pii = pair<int, int>;
+using pll = pair<lld, lld>;
+
+int solve (string members, string fans) {
+    for (int i = )
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int c;
+    cin >> c;
+    while (c--) {
+        string members, fans;
+        cin >> members, fans;
+        cout << solve(members, fans) << endl;
+    }
+
+    return 0;
+}
