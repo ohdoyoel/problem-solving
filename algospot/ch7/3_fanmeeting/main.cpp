f99d3d7accc1f0d3882fd88bd494fcfe345d786d
Daniel Oh <ohdoyoel@gmail.com>
2023-12-25 00:09:41 +0900

    algospot fanmeeting

--- algospot/ch7/3_fanmeeting/main.cpp
+++ algospot/ch7/3_fanmeeting/main.cpp
@@ -7,8 +7,34 @@ using lld = long long;
 using pii = pair<int, int>;
 using pll = pair<lld, lld>;
 
-int solve (string members, string fans) {
-    for (int i = )
+void addTo(vector<int> &a, vector<int> &b, int k) {
+    int an = sz(a), bn = sz(b);
+    if (bn == 0) return an;
+
+    for (int i = k; i < bn; ++i) a[i] += b[i - k];
+}
+
+vector<int> karatsuba(vector<int> &a, vector<int> &b) {
+    int an = sz(a), bn = sz(b);
+    if (an < bn) return karatsuba(b, a);
+    if (an == 0 || bn == 0) return vector<int> ();
+    
+    int midIdx = an / 2;
+    vector<int> a0(a.begin(), a.begin() + midIdx);
+    vector<int> a1(a.begin() + midIdx, a.end());
+    vector<int> b0(b.begin(), b.begin() + min(sz(b), midIdx));
+    vector<int> b1(b.begin() + min(sz(b), midIdx), b.end());
+
+    vector<int> z2 = zaratsuba(a1, b1);
+    vector<int> z0 = zaratsuba(a0, b0);
+
+    addTo(a0, a1, 0);
+    addTo(b0, b1, 0);
+    
+}
+
+int hugCnt (string members, string fans) {
+    
 }
 
 int main() {
@@ -20,7 +46,7 @@ int main() {
     while (c--) {
         string members, fans;
         cin >> members, fans;
-        cout << solve(members, fans) << endl;
+        cout << sz(fans) - sz(members) + 1 - hugCnt(members, fans) << endl;
     }
 
     return 0;
