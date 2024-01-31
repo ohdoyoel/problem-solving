da246abbef889f6cbd326da93d55301d25626fc1
Daniel Oh <ohdoyoel@gmail.com>
2024-01-31 23:35:59 +0900

    wildcard

--- algospot/ch8/1_wildcard/main.cpp
+++ algospot/ch8/1_wildcard/main.cpp
@@ -14,45 +14,57 @@ bool isOnlyHaveStar(string s) {
     return true;
 }
 
+string pieceToFind(string w) {
+    int starIdx = sz(w);
+    for (int i = 0; i < sz(w); ++i) {
+        if (w[i] == '*') {
+            starIdx = i;
+            break;
+        }
+    }
+    return w.substr(0, starIdx);
+}
+
 bool patternMatched(string wildcard, string filename) {
     int pos = 0;
     while ((pos < sz(wildcard) && pos < sz(filename)) && (wildcard[pos] == '?' || wildcard[pos] == filename[pos])) {
         pos++;
-        if (pos == sz(wildcard)) {
-            return pos == sz(filename);
-        }
-        if (pos == sz(filename)) {
-            return isOnlyHaveStar(wildcard.substr(pos));
-        }
-        if (wildcard[pos] == '*') {
-            string piece = wildcard[pos + 1 : '*'가 나오기 전];
-            int skip = filename.find(piece, pos);
-            if (skip == -1) {
-                return false;
-            } else {
-                return patternMatched(wildcard.substr(pos + 1), filename.substr(pos + skip));
+    }
+    
+    if (pos == sz(wildcard)) return (pos == sz(filename));
+    if (pos == sz(filename)) return isOnlyHaveStar(wildcard.substr(pos));
+
+    if (wildcard[pos] == '*') {
+        for (int skip = 0; skip < sz(filename) - pos; ++skip) {
+            if (patternMatched(wildcard.substr(pos + 1), filename.substr(pos + 1 + skip))) {
+                return true;
             }
         }
     }
-
-    return true;
+    
+    return false;
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-
+    
     int c;
     cin >> c;
     while (c--) {
         string w;
         cin >> w;
+        vector<string> result;
         int n;
         cin >> n;
         while (n--) {
             string f;
             cin >> f;
-            if (patternMatched(w, f)) cout << f << endl;
+            if (patternMatched(w, f)) result.push_back(f);
+        }
+        sort(result.begin(), result.end());
+        for (auto e:result) {
+            cout << e << endl;
         }
     }
 
