80ffdafd324bc7be1dc668a62c8942e7b3ec7eb0
Daniel Oh <ohdoyoel@gmail.com>
2024-02-09 20:37:34 +0900

    boj 2579

--- boj/2579/main.cpp
+++ boj/2579/main.cpp
@@ -12,24 +12,22 @@ vector<int> stairs;
 
 int score(int n, int s, int c) {
     int& ret = cache[n][s][c];
-    if (ret != -1) return ret;
+    if (ret != 0) return ret;
     
-    if (n > sz(stairs) - 1) return ret = -1;
+    if (n > sz(stairs) - 1) return ret = -987654321;
     if (n == sz(stairs) - 1) return ret = stairs[n];
-    
-    if (score(n + s, 1, c) < score(n + s, 2, c) || c == 1) {
-        c = 0;
-        return ret = stairs[n] + score(n + s, 2, c);
+
+    if (c == 1 || score(n + s, 1, c + 1) < score(n + s, 2, c)) {
+        return ret = stairs[n] + score(n + s, 2, 0);
     } else {
-        c++;
-        return ret = stairs[n] + score(n + s, 1, c);
+        return ret = stairs[n] + score(n + s, 1, c + 1);
     }
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    memset(cache, -1, sizeof(cache));
+    memset(cache, 0, sizeof(cache));
 
     int n;
     cin >> n;
@@ -38,7 +36,11 @@ int main() {
         cin >> stairs[i];
     }
     
-    cout << max(score(0, 1, 0), score(0, 2, 0)) << endl;
+    cout << max({score(0, 1, 1), score(0, 2, 0), score(1, 1, 1), score(1, 2, 0)}) << endl;
+    // cout << score(0, 1, 1) << endl;
+    // cout << score(0, 2, 0) << endl;
+    // cout << score(1, 1, 1) << endl;
+    // cout << score(1, 2, 0) << endl;
 
     return 0;
 }
