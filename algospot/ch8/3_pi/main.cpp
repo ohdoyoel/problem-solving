bed4d927457a5ea19986f7c28f01791ecf6aa4e9
Daniel Oh <ohdoyoel@gmail.com>
2024-02-19 23:55:40 +0900

    algospot pi

--- algospot/ch8/3_pi/main.cpp
+++ algospot/ch8/3_pi/main.cpp
@@ -7,8 +7,8 @@ using lld = long long;
 using pii = pair<int, int>;
 using pll = pair<lld, lld>;
 
-string numbers = "";
-int cache[10001];
+string numbers;
+int cache[10000];
 
 bool allNumbersSame(string n) {
     for (int i = 0; i < sz(n); ++i) {
@@ -27,37 +27,51 @@ bool numbersBlink(string n) {
 
 bool numbersIncOrDecByOne(string n) {
     for (int i = 1; i < sz(n); ++i) {
-        if (n[0] < n[sz(n) - 1] && n[i - 1] + 1 != n[i]) return false;
-        else if (n[0] > n[sz(n) - 1] && n[i - 1] - 1 != n[i]) return false;
+        if (n[0] < n[sz(n) - 1] && 1 != n[i] - n[i - 1]) return false;
+        else if (n[0] > n[sz(n) - 1] && -1 != n[i] - n[i - 1]) return false;
     }
     return true;
 }
 
 bool isNumbersArithmSeq(string n) {
-    for (int i = 2; i < sz(n); ++i) {
-        if (n[i - 1] - n[i - 2] != n[i] - n[i - 1]) return false;
+    for (int i = 1; i < sz(n); ++i) {
+        if (n[1] - n[0] != n[i] - n[i - 1]) return false;
     }
     return true;
 }
 
 int level(string n) {
-    if (allNumbersSame(n)) return 1;
-    if (numbersBlink(n)) return 4;
-    if (numbersIncOrDecByOne(n)) return 2;
-    if (isNumbersArithmSeq(n)) return 5;
+    // if (allNumbersSame(n)) return 1;
+    // if (numbersBlink(n)) return 4;
+    // if (numbersIncOrDecByOne(n)) return 2;
+    // if (isNumbersArithmSeq(n)) return 5;
+    // return 10;
+    
+    if (n == string(sz(n), n[0])) return 1;
+    
+    bool progressive = true;
+    for (int i = 1; i < sz(n); ++i)
+        if (n[1] - n[0] != n[i] - n[i - 1]) progressive = false;
+    if (progressive && abs(n[1] - n[0]) == 1) return 2;
+    else if (progressive) return 5;
+    
+    bool alternave = true;
+    for (int i = 0; i < sz(n); ++i)
+        if (n[i] != n[i%2]) alternave = false;
+    if (alternave) return 4;
+    
     return 10;
 }
 
 int minLevel(int start) {
+    if (start == sz(numbers)) return 0;
+    
     int &res = cache[start];
-    if (res != 987654321) return res;
-
-    if (3 <= sz(numbers) - start && sz(numbers) - start <= 5) return res = level(numbers.substr(start));
-    // else if (end - start < 3) return res = 987654321;
-
-    // res = 987654321;
-    for (int s = 3; s <= 5; ++s) {
-        res = min(res, level(numbers.substr(start, start + s)) + minLevel(start + s));
+    if (res != -1) return res;
+    
+    res = 987654321;
+    for (int s = 3; s <= 5 && start + s <= sz(numbers); ++s) {
+        res = min(res, level(numbers.substr(start, s)) + minLevel(start + s));
     }
     return res;
 }
@@ -69,8 +83,7 @@ int main() {
     int c;
     cin >> c;
     while (c--) {
-        for (int i = 0; i < 10001; ++i)
-            cache[i] = 987654321;
+        memset(cache, -1, sizeof(cache));
         cin >> numbers;
         cout << minLevel(0) << endl;
     }
