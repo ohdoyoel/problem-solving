08a75741c471b1f3a81ab6732ac45d17e928df08
Daniel Oh <ohdoyoel@gmail.com>
2024-02-24 22:34:22 +0900

    boj 2225

--- boj/2225/main.cpp
+++ boj/2225/main.cpp
@@ -9,18 +9,21 @@ using pll = pair<lld, lld>;
 
 int cache[201][201];
 
+// 0~n 중 k개를 뽑았을 때 합이 n인 경우의 수
 int solve(int n, int k) {
-    if (n == 0) return 1;
 
     int& ret = cache[n][k];
     if (ret != -1) return ret;
     
+    if (k == 0) return ret = 0;
     if (k == 1) return ret = 1;
     if (k == 2) return ret = n + 1;
+    if (n == 0) return ret = 1;
 
     ret = 0;
     for (int i = 0; i <= n; ++i) {
         ret += solve(n - i, k - 1);
+        ret %= 1000000000;
     }
     return ret % 1000000000;
 }
@@ -29,5 +32,5 @@ int main() {
     memset(cache, -1, sizeof(cache));
     int n, k;
     cin >> n >> k;
-    cout << solve(n, k) % 1000000000 << endl;
+    cout << solve(n, k) << endl;
 }
