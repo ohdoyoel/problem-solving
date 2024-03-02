1deaad7c2009548a8bd05ead26076a76cea6cff9
Daniel Oh <ohdoyoel@gmail.com>
2024-03-02 22:56:48 +0900

    algospot quantize

--- algospot/ch8/4_quantize/main.cpp
+++ algospot/ch8/4_quantize/main.cpp
@@ -4,32 +4,34 @@
 #define inf 987654321
 using namespace std;
 
-int cache[101][101][11];
+int cache[101][11];
 vector<int> seq;
 
 // start부터 end까지의 수열 중 정수로 대응된 LSS
 int lss(int start, int end) {
     if (seq[start] == seq[end - 1]) return 0;
     
-    int ret = inf;
-    for (int v = seq[start]; v <= seq[end - 1]; ++v) {
-        int sse = 0;
-        for (int i = start; i < end; i++) sse += pow(seq[i] - v, 2);
-        ret = min(ret, sse);
-    }
-    return ret;
+    double avrg = 0;
+    for (int i = start; i < end; ++i) avrg += seq[i];
+    avrg /= (end - start);
+
+    int sse = 0;
+    for (int i = start; i < end; i++) sse += pow(seq[i] - round(avrg), 2);
+    
+    return sse;
 }
 
 // seq를 s개로 양자화했을 때 나오는 LSS
-int solve(int start, int end, int s) {
-    int& ret = cache[start][end][s];
+int solve(int start, int s) {
+    if (start == sz(seq)) return 0;
+    if (s == 0) return inf;
+  
+    int& ret = cache[start][s];
     if (ret != -1) return ret;
 
-    if (s == 0) return ret = 0;
-
     ret = inf;
-    for (int l = start + 1; l <= end; ++l)
-        ret = min(ret, lss(start, l) + solve(l, end, s - 1));
+    for (int l = start + 1; l <= sz(seq); ++l)
+        ret = min(ret, lss(start, l) + solve(l, s - 1));
     return ret;
 }
 
@@ -43,7 +45,6 @@ int main() {
         seq.resize(n);
         for (int i = 0; i < n; ++i) cin >> seq[i];
         sort(seq.begin(), seq.end());
-        cout << solve(0, 3, s) << endl;
-        // cout << solve(0, sz(seq), s) << endl;
+        cout << solve(0, s) << endl;
     }
 }
