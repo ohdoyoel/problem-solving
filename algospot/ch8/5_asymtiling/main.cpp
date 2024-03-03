c8c23ce20250f32323c9bb461e56663ce84e9b28
Daniel Oh <ohdoyoel@gmail.com>
2024-03-03 23:08:22 +0900

    algospot asymtiling

--- algospot/ch8/5_asymtiling/main.cpp
+++ algospot/ch8/5_asymtiling/main.cpp
@@ -2,29 +2,25 @@
 #define sz(x) (uint)(x).size()
 #define endl "\n"
 using namespace std;
+using lld = long long int;
 
-int cache[101];
+lld fib[101];
 
-int solve(int n) {
-    int& ret = cache[n];
-    if (ret != -1) return ret;
-    
-    if (n == 1) return 0;
-    else if (n == 2) return 0;
-    else if (n == 3) return 2;
-    
+int main() {    
+    fib[0] = 1;
+    fib[1] = 1;
+    fib[2] = 2;
+    for (int i = 3; i < 101; ++i) fib[i] = (fib[i - 1] + fib[i - 2]) % 1000000007;
 
-    if (n % 2) return ret= solve(n - 1) + solve(n - 2) - solve(n / 2) - solve(n / 2 - 1);
-    return ret = solve(n - 1) + solve(n - 2) - solve(n / 2);
-}
-
-int main() {
-    memset(cache, -1, sizeof(cache));
     int c;
     cin >> c;
     while(c--) {
         int n;
         cin >> n;
-        cout << solve(n) << endl;
+        lld res = fib[n];
+        if (n % 2) res -= fib[n / 2];
+        else res -= ((fib[n / 2 - 1] + fib[n / 2]) % 1000000007);
+        if (res < 0) res += 1000000007;
+        cout << res << endl;
     }
 }
