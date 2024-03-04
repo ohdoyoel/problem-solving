fe55c41e555c5d3079caf9829de0e20f5a2b0104
Daniel Oh <ohdoyoel@gmail.com>
2024-03-04 22:36:24 +0900

    boj 11054

--- boj/11054/main.cpp
+++ boj/11054/main.cpp
@@ -8,7 +8,7 @@ int cacheDec[1001];
 int n;
 vector<int> A;
 
-// 0 ~ to까지 증가하는 부분 수열
+// 0 ~ to까지 증가하는 부분 수열 중 to를 포함하고 가장 긴 길이
 int inc(int to) {
     if (to < 0) return 0;
     
@@ -16,11 +16,11 @@ int inc(int to) {
     if (ret != -1) return ret;
 
     ret = 1;
-    for (int i = 0; i < to; ++i) if (A[i] < A[to]) ret = max(ret, 1 + inc(i - 1));
+    for (int i = to; i >= 0; --i) if (A[i] < A[to]) ret = max(ret, inc(i) + 1);
     return ret;
 }
 
-// from ~ 끝까지 감소하는 부분 수열
+// from ~ 끝까지 감소하는 부분 수열 중 from을 포함하고 가장 긴 길이
 int dec(int from) {
     if (from > 1000) return 0;
   
@@ -28,16 +28,16 @@ int dec(int from) {
     if (ret != -1) return ret;
 
     ret = 1;
-    for (int i = from; i < n; ++i) if (A[from] > A[i]) ret = max(ret, 1 + dec(i + 1));
+    for (int i = from; i < n; ++i) if (A[from] > A[i]) ret = max(ret, 1 + dec(i));
     return ret;
 }
 
-// from부터 끝까지의 가장 긴 바이토닉 부분 수열
+// 가장 긴 바이토닉 부분 수열
 int solve() {
     int ret = 1;
     for (int top = 0; top < n; ++top) {
         ret = max(ret, inc(top) + dec(top) - 1);
-        cout << inc(top) + dec(top) - 1 << endl;
+        // cout << inc(top) << " " << dec(top) << endl;
     }
     return ret;
 }
