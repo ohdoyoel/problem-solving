5afb2fbfe613a95bde2ee7b36ce427e0b6281d6d
Daniel Oh <ohdoyoel@gmail.com>
2024-03-08 21:21:09 +0900

    boj 10942 time failed

---
+++ boj/10942/main.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int cache[2001][2001];
+int n;
+vector<int> seq;
+
+// s~e까지 팰린드롬인지 반환
+bool isPldrm(int start, int end) {
+    int &ret = cache[start][end];
+    if (ret != -1) return ret;
+
+    if (start >= end) return ret = true;
+
+    return ret = (seq[start] == seq[end]) && isPldrm(start+1, end-1);
+}
+
+int main() {
+    memset(cache, -1, sizeof(cache));
+    cin >> n; seq.resize(n);
+    for (int i = 0; i < n; ++i) cin >> seq[i];
+    int m; cin >> m;
+    while(m--) {
+        int s, e; cin >> s >> e;
+        cout << isPldrm(s-1, e-1) << endl;
+    }
+}
