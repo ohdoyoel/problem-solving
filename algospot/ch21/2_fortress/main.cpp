d39d096db364849a42d265735fb9c7d27e82bb07
Daniel Oh <ohdoyoel@gmail.com>
2024-03-11 23:24:04 +0900

    algospot fortress

---
+++ algospot/ch21/2_fortress/main.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main() {
+
+}
+
+// 큰 성벽이 부모가 되고, 그 안의 성벽은 자식이 되도록 (계층 구조를 표현!)
+// 자식 트리의 개수가 1 -> depth 구하기
+// 1 이상 -> 큰 depth 두개의 합
