1a5c132217da1a67da20e4361369827a8f88e2fc
Daniel Oh <ohdoyoel@gmail.com>
2024-03-13 23:03:27 +0900

    boj 1406

--- boj/1406/main.cpp
+++ boj/1406/main.cpp
@@ -9,18 +9,17 @@ int main() {
     for(char c : line) cout << c;
     cout << endl;
     int n; cin >> n;
-    int cursur = 0;
+    list<char>::iterator cursur = line.end();
     while(n--) {
         char cmd; cin >> cmd; cout << cmd;
-        if (cmd == 'L') if (cursur > 0) cursur--;
-        else if (cmd == 'D') if (cursur <= n) cursur++;
-        else if (cmd == 'B') if (cursur > 0) {
-            line.erase(next(line.begin(), cursur-1));
-            cursur--;
+        if (cmd == 'L') if (cursur > line.begin()) cursur--;
+        else if (cmd == 'D') if (cursur < line.end()) cursur++;
+        else if (cmd == 'B') {
+            if (cursur > line.begin()) line.erase(--cursur);
         }
         else if (cmd == 'P') {
             char x; cin >> x;
-            line.insert(next(line.begin(), cursur), x);
+            line.insert(cursur, x);
         }
         for(char c : line) cout << c;
         cout << endl;
