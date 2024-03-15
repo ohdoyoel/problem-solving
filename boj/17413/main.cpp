e11e27313944f6195be5523661ae8dad74303dd8
Daniel Oh <ohdoyoel@gmail.com>
2024-03-15 22:48:39 +0900

    boj 17413

--- boj/17413/main.cpp
+++ boj/17413/main.cpp
@@ -6,8 +6,20 @@ stack<char> s;
 
 int main() {
     string str; getline(cin,str);
-    for (int i=0; i<str.size(); i++) {
-        if (str[i] == '<') {
+    for (int i=0; i<=str.size(); i++) {
+        if (str[i]==' ' || i==str.size()) {
+            while(!s.empty()) {
+                cout << s.top();
+                s.pop();
+            }
+            if (i!=str.size()) cout << str[i];
+        }
+        
+        else if (str[i] == '<') {
+            while(!s.empty()) {
+                cout << s.top();
+                s.pop();
+            }
             while(str[i] != '>') {
                 cout << str[i];
                 i++;
@@ -15,19 +27,6 @@ int main() {
             cout << str[i];
         }
         
-        else if (str[i]==' ' || str[i]=='<') {
-            while(!s.empty()) {
-                cout << s.top();
-                s.pop();
-            }
-            cout << str[i];
-        } else s.push(str[i]);
+        else s.push(str[i]);
     }
-    
-    while(!s.empty()) {
-        cout << s.top();
-        s.pop();
-    }
-    
-    cout << ' ';
 }
