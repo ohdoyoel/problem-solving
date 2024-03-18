c0ce25ad19cea5c728294cced259146347670a59
Daniel Oh <ohdoyoel@gmail.com>
2024-03-19 00:00:04 +0900

    edit

--- boj/30885/main.cpp
+++ boj/30885/main.cpp
@@ -9,7 +9,7 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
-    int n; cin >> n;
+    int n ; cin >> n;
     for(int i=1; i<=n; i++) {
         int a; cin >> a;
         A.push_back(make_pair(a, i));
