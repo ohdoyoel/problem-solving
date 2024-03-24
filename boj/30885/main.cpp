8dce43503182e47dd26fedc63a39260c6690d245
Daniel Oh <ohdoyoel@gmail.com>
2024-03-24 13:07:08 +0900

    boj 30885

--- boj/30885/main.cpp
+++ boj/30885/main.cpp
@@ -1,9 +1,10 @@
 #include<bits/stdc++.h>
 #define endl "\n"
 using namespace std;
-using pii = pair<int,int>;
+using lld = long long int;
+using pli = pair<lld,int>;
 
-list<pii> A;
+list<pli> A;
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -11,20 +12,27 @@ int main() {
 
     int n; cin >> n;
     for(int i=1; i<=n; i++) {
-        int a; cin >> a;
+        lld a; cin >> a;
         A.push_back(make_pair(a, i));
     }
 
     while(A.size() != 1) {
         for (auto it=A.begin(); it!=A.end(); ++it) {
-            if (it!=A.begin() && next(it, -1)->first <= it->first){
+            if (it!=A.begin() && next(it, -1)->first <= it->first
+                && it!=next(A.end(), -1) && it->first >= next(it, 1)->first){
+                it->first += next(it, -1)->first;
+                it->first += next(it, 1)->first;
+                A.erase(next(it, -1));
+                A.erase(next(it, 1));
+            } else if (it!=A.begin() && next(it, -1)->first <= it->first){
                 it->first += next(it, -1)->first;
                 A.erase(next(it, -1));
-            }
-            if (it!=next(A.end(), -1) && it->first >= next(it, 1)->first) {
+            } else if (it!=next(A.end(), -1) && it->first >= next(it, 1)->first) {
                 it->first += next(it, 1)->first;
                 A.erase(next(it, 1));
             }
+            // for (pii a : A) cout << a.first << " ";
+            // cout << endl;
         }
     }
 
