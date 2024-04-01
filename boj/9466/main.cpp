03c6bbe5de03d8bf43c31f23b4850b3a1417fa7a
Daniel Oh <ohdoyoel@gmail.com>
2024-04-01 20:20:05 +0900

    boj 9466 failed

--- boj/9466/main.cpp
+++ boj/9466/main.cpp
@@ -1,21 +1,31 @@
 #include<bits/stdc++.h>
 #define endl "\n"
+#define all(x) (x).begin(), (x).end()
 using namespace std;
 
 int n;
-vector<int> to, log;
+vector<int> to, lg;
 vector<bool> visited;
 
 void dfsTeam(int v) {
+    // cout << v << endl;
+    auto vIt = find(all(lg), v);
+    if (vIt != lg.end()) {
+        for (auto it=lg.begin(); it!=vIt; it++) {
+            // cout << v << " " << *it << endl;
+            visited[*it] = false;
+        }
+        return;
+    }
+    
     visited[v] = true;
-    log.push_back(v);
-    // ... return;
-    visited[v] = false;
-    log.pop_back();
+    lg.push_back(v);
+    dfsTeam(to[v]);
 }
 
 int dfsNotTeam() {
     for (int v=1; v<=n; v++) {
+        lg.clear();
         if (!visited[v]) dfsTeam(v);
     }
     
@@ -27,10 +37,13 @@ int dfsNotTeam() {
 }
 
 int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int t; cin>>t;
     while(t--) {
         cin>>n; to.resize(n+1); visited.resize(n+1);
-        for (int i=1; i<=n; i++) cin>>to[i];
+        for (int i=1; i<=n; i++) {
+            cin>>to[i]; visited[i]=false;
+        }
         cout << dfsNotTeam() << endl;
     }
 }
