9e9574e0702eed21be5992da41ffd8e5f07e4a93
Daniel Oh <ohdoyoel@gmail.com>
2024-06-20 23:13:44 +0900

    boj 9934

--- boj/9934/main.cpp
+++ boj/9934/main.cpp
@@ -5,17 +5,27 @@ using namespace std;
 
 int k;
 vector<int> A;
-vector<int> tree;
+vector<vector<int>> tree;
 
 void fillTree(int start, int end, int depth) {
     if (depth == k) return;
 
     int mid = (start+end)/2;
-    tree[]
+    tree[depth].push_back(A[mid]);
+    
+    fillTree(start, mid, depth+1);
+    fillTree(mid+1, end, depth+1);
+}
+
+void printTree() {
+    for (vector<int> level : tree) {
+        for (int child : level) cout << child << " ";
+        cout << endl;
+    }
 }
 
 int main() {
-    cin >> k; A.resize(pow(2, k)-1); tree.resize(pow(2, k)-1);
+    cin >> k; A.resize(pow(2, k)-1); tree.resize(k);
     for (int i=0; i<sz(A); i++) cin >> A[i];
     fillTree(0, sz(A), 0);
     printTree();
