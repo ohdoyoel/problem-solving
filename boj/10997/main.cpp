4793168933d1b90848a87bf869fe9654ee28d150
Daniel Oh <ohdoyoel@gmail.com>
2024-07-08 22:46:44 +0900

    boj 10997

---
+++ boj/10997/main.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+vector<vector<int>> board;
+
+int main() {
+    int n; cin >> n;
+    if (n == 1) {
+        cout << '*' <<< endl;
+        return;
+    }
+    
+}
