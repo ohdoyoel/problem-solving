d3e1a288dc5e52b004941e6e9508c1b753bd4686
Daniel Oh <ohdoyoel@gmail.com>
2024-08-30 23:18:41 +0900

    algospot fence main2

--- algospot/ch7/2_fence/main2.cpp
+++ algospot/ch7/2_fence/main2.cpp
@@ -6,23 +6,29 @@ int n;
 vector<int> A;
 
 int maxArea(int b, int e) {
-    if (e-b==1) return A[b];
+    if (b==e) return A[b];
 
     int m = (b+e) / 2;
     int leftArea = maxArea(b, m);
-    int rightArea = maxArea(m, e);
+    int rightArea = maxArea(m+1, e);
+    int ret = max(leftArea, rightArea);
 
     int leftEnd = m; int rightEnd = m+1;
-    int pivot = A[leftEnd];
-    int midArea = pivot;
-    while(b <= leftEnd && rightEnd <= e) {
-        pivot = min({A[leftEnd], pivot, A[rightEnd]});
-        if (b <= leftEnd && A[leftEnd-1] >= pivot) leftEnd--;
-        else if (rightEnd <= e && pivot <= A[rightEnd+1]) rightEnd++;
-        midArea = max(pivot*(rightEnd-leftEnd), midArea);
+    int pivot = min(A[leftEnd], A[rightEnd]);
+    while (b <= leftEnd-1 && A[leftEnd-1] >= pivot) leftEnd--;
+    while (rightEnd+1 <= e && pivot <= A[rightEnd+1]) rightEnd++;
+    int midArea = pivot*(rightEnd-leftEnd+1);
+    
+    while(b <= leftEnd-1 || rightEnd+1 <= e) {
+        if(A[leftEnd-1] < pivot && pivot > A[rightEnd+1]) pivot = max(A[leftEnd-1], A[rightEnd+1]);
+        else if (A[leftEnd-1] < pivot) pivot = A[leftEnd-1];
+        else if (A[rightEnd+1] < pivot) pivot = A[rightEnd+1];
+        while (b <= leftEnd-1 && A[leftEnd-1] >= pivot) leftEnd--;
+        while (rightEnd+1 <= e && pivot <= A[rightEnd+1]) rightEnd++;
+        midArea = max(pivot*(rightEnd-leftEnd+1), midArea);
     }
 
-    return max({leftArea, midArea, rightArea});
+    return max(ret, midArea);
 }
 
 int main() {
@@ -30,6 +36,6 @@ int main() {
     while (c--) {
         cin >> n; A.resize(n);
         for (int i=0; i<n; i++) cin >> A[i];
-        cout << maxArea(0, n) << endl;
+        cout << maxArea(0, n-1) << endl;
     }
 }
