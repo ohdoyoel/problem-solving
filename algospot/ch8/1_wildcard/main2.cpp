fe707615faece020960c6a75f93415adf020913d
Daniel Oh <ohdoyoel@gmail.com>
2024-09-05 22:28:15 +0900

    algospot wildcard failed

--- algospot/ch8/1_wildcard/main2.cpp
+++ algospot/ch8/1_wildcard/main2.cpp
@@ -3,32 +3,29 @@
 using namespace std;
 
 string wild, src;
-int cache[101][101]
+int cache[101][101];
 
 int isMatched(int w, int s) {
-    if (w == wild.size() || s == src.size()) return 0;
+    if (w == wild.size()) return s == src.size();
 
     int& ret = cache[w][s];
     if (ret != -1) return ret;
 
-    if (wild[w] == '?') return ret = isMatched(w+1, s+1);
-    else if (wild[w] == '*') {
-        for (int next=w; next<wild.size(); amt++) {
-            if (isMatched(w+1, s+ne))
-        }
-    } else if (wild[w] == src[s]) return ret = isMatched(w+1, s+1);
-    else return ret = 0;
+    if (wild[w] == '?' || wild[w] == src[s]) return ret = isMatched(w+1, s+1);
+    else if (wild[w] == '*') for (int next=s; next<=src.size(); next++) if (isMatched(w+1, next)) return ret = 1;
+    
+    return ret = 0;
 }
 
 int main() {
-    memset(cache, -1, sizeof(cache));
     vector<string> result;
-    int c; cin >> n;
+    int c; cin >> c;
     while (c--) {
         cin >> wild;
         int n; cin >> n;
         while (n--)  {
             cin >> src;
+            memset(cache, -1, sizeof(cache));
             if (isMatched(0, 0)) result.push_back(src);
         }
     }
