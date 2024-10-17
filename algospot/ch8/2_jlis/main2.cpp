7647d190ed2b445bd6f77d5f0888523ff1d1edd0
Daniel Oh <ohdoyoel@gmail.com>
2024-10-17 18:32:05 +0900

    algospot jlis main2 success

--- algospot/ch8/2_jlis/main2.cpp
+++ algospot/ch8/2_jlis/main2.cpp
@@ -7,24 +7,22 @@ vector<int> B;
 int cache[102][102];
 
 // A[i], B[j] 중 1개를 포함한 이후의 jlis
+// 따라서 i, j 중 하나가 -1이면 그 배열은 아직 선택X, 둘다 -1이 아니면 큰놈이 선택된 것
 int jlis(int i, int j) {
-    if (i==A.size() || j==B.size()) return -987654321;
-
     int& ret = cache[i+1][j+1];
     if (ret != -1) return ret;
     
-    int tail = 0;
-    if (i!=-1 && j!=-1) tail = max(A[i], B[j]);
-    else if (i==-1) tail = B[j];
-    else if (j==-1) tail = A[i];
-
-    int rest = 0;
-    for (int a=i+1; a<A.size(); a++) if (tail < A[a]) rest = max(rest, jlis(a, j));
-    for (int b=j+1; b<B.size(); b++) if (tail < B[b]) rest = max(rest, jlis(i, b));
-    return ret = 1 + rest;
+    int tail = (i==-1 ? B[j] : (j==-1 ? A[i] : max(A[i], B[j])));
+    
+    ret = 1;
+    for (int a=i+1; a<A.size(); a++) if (tail < A[a]) ret = max(ret, 1+jlis(a, j));
+    for (int b=j+1; b<B.size(); b++) if (tail < B[b]) ret = max(ret, 1+jlis(i, b));
+    return ret;
 }
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
     int c; cin >> c;
     while (c--) {
         memset(cache, -1, sizeof(cache));
@@ -32,6 +30,10 @@ int main() {
         cin >> a >> b; A.resize(a); B.resize(b);
         for (int i=0; i<a; i++) cin >> A[i];
         for (int i=0; i<b; i++) cin >> B[i];
-        cout << jlis(-1, -1) - 1 << endl;
+        int res = 0;
+        for (int i=0; i<a; i++) res = max(res, jlis(i, -1));
+        for (int j=0; j<b; j++) res = max(res, jlis(-1, j));
+        cout << res << endl;
     }
+    return 0;
 }
