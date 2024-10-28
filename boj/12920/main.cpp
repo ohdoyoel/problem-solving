ca06ca692a1b5686fe5bb2128f33a73e31f187a0
Daniel Oh <ohdoyoel@gmail.com>
2024-10-28 16:36:22 +0900

    boj 12920 timeout

---
+++ boj/12920/main.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int cache[101]10001];
+int n, m;
+vector<int> V, C, K;
+
+int contentment(int i, int volume) {
+    if (volume<0) return -987654321;
+    if (i==n) return 0;
+
+    int& ret = cache[i][volume];
+    if (ret!=-1) return ret;
+
+    ret = 0;
+    for (int j=0; j<=K[i]; ++j) ret = max(ret, j*C[i] + contentment(i+1, volume-j*V[i]));
+    return ret;
+}
+
+int main() {
+    ios_base::syn_with_stdio(0); cin.tie(0); cout.tie(9);
+    memset(cache, -1, sizeof(cache));
+    cin >> n >> m;
+    for (int i=0; i<n; ++i) cin >> V[i] >> C[i] >> K[i];
+    cout << contentment(0, m) << endl;
+}
