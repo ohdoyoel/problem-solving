66a1026a2ee0065e3ac40a5b6f4198ca304d8d0b
Daniel Oh <ohdoyoel@gmail.com>
2024-11-03 19:26:14 +0900

    boj 2098

--- boj/2098/main.cpp
+++ boj/2098/main.cpp
@@ -2,36 +2,38 @@
 #define endl "\n"
 using namespace std;
 
+int cache[17][1<<16][17];
 int n;
 vector<vector<int>> W;
-vector<int> P;
-vector<bool> V;
+uint visited;
 
-long long path(int here) {
-    if (P.size()==n) return W[here][P.front()]==0 ? 987654321987654321 : W[here][P.front()];
+int path(int here, uint visited, int start) {
+    if (visited==(1<<n)-1) return W[here][start]==0 ? 987654321 : W[here][start];
+    
+    int& ret = cache[here+1][visited][start+1];
+    if (ret!=-1) return ret;
 
-    long long ret = 987654321987654321;
+    ret = 987654321;
     for (int next=0; next<n; ++next) {
-        if (V[next]) continue;
+        if (visited & (1<<next)) continue;
 
-        P.push_back(next);
-        V[next] = true;
+        visited |= (1<<next);
         
-        if (here==-1) ret = min(ret, path(next));
-        else if (W[here][next]!=0) ret = min(ret, W[here][next]+path(next));
+        if (here==-1) ret = min(ret, path(next, visited, next));
+        else if (W[here][next]!=0) ret = min(ret, W[here][next] + path(next, visited, start));
         
-        P.pop_back();
-        V[next] = false;
+        visited &= ~(1<<next);
     }
     return ret;
 }
 
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    cin >> n; W.resize(n); V.resize(n);
+    memset(cache, -1, sizeof(cache));
+    cin >> n; W.resize(n);
     for (int i=0; i<n; ++i) {
         W[i].resize(n);
         for (int j=0; j<n; ++j) cin >> W[i][j];
     }
-    cout << path(-1) << endl;;
+    cout << path(-1, 0, -1) << endl;;
 }
