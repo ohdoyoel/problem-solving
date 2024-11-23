7b9fdc793141aaae71bcc544b18664f9674d6725
Daniel Oh <ohdoyoel@gmail.com>
2024-11-23 15:17:49 +0900

    algospot restore failed

--- algospot/ch9/6_restore/sol.cpp
+++ algospot/ch9/6_restore/sol.cpp
@@ -9,7 +9,7 @@ int cache[16][1<<15];
 
 int overlapLen(string& a, string& b) {
     int maxLen = min(a.size(), b.size());
-    if (a.find(b) != string::npos) return maxLen;
+    if (a.find(b)!=string::npos || b.find(a)!=string::npos) return maxLen;
     for (int len=maxLen; len>=0; --len) if (a.substr(a.size()-len) == b.substr(0, len)) return len;
 }
 
@@ -29,6 +29,16 @@ int restore(int last, int used) {
     return ret;
 }
 
+string reconstruct(int last, int used) {
+    if (used == (1<<k)-1) return "";
+    for (int next=0; next<k; ++next) {
+        if (used & (1<<next)) continue;
+        int ifUsed = last==-1 ? restore(next, used|(1<<next)) : restore(next, used|(1<<next)) + overlap[last][next];
+        if (ifUsed == restore(last, used)) return last==-1 ? ss[next]+reconstruct(next, used|(1<<next)) : ss[next].substr(overlap[last][next])+reconstruct(next, used|(1<<next));
+    }
+    return "**oops**";
+}
+
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int c; cin >> c;
@@ -41,6 +51,11 @@ int main() {
             n += ss[i].size();
         }
         for (int i=0; i<k; ++i) for (int j=0; j<k; ++j) overlap[i][j] = overlapLen(ss[i], ss[j]);
-        cout << n - restore(-1, 0) << endl;
+        // for (int i=0; i<k; ++i){
+        //   for (int j=0; j<k; ++j) cout << overlap[i][j] << ' ';
+        //   cout << endl;
+        // }
+        // cout << n - restore(-1, 0) << endl;
+        cout << reconstruct(-1, 0) << endl;
     }
 }
