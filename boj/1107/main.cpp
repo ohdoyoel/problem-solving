6f7ca6adf88442ee1c7a308630c9c98c4ab9aaed
Daniel Oh <ohdoyoel@gmail.com>
2024-12-18 17:32:11 +0900

    boj 1107

--- boj/1107/main.cpp
+++ boj/1107/main.cpp
@@ -3,53 +3,30 @@
 using namespace std;
 
 int n;
-// A[0~9] 가능 여부, A[10] = '+', A[11] = '-' 의 가능 여부
 vector<bool> P;
-vector<int> A;
 
-int AtoNum() {
-    // if (A.size()>1 && A[0]==5 && A[1]==4) {
-    // for (auto a : A) cout << a << ' ';
-    // cout << endl;
-    // }
-    int ret = 100;
-    for (int i=0; i<A.size(); ++i) {
-        int a = A[i];
-        if (a==10) ret++;
-        else if (a==11) if (ret>0) ret--;
-        else {
-            if (i>0 && A[i-1]<10) {
-                ret *= 10;
-                ret += a;
-            } else {
-                ret = a;
-            }
-        }
-    }
-    return ret;
+bool makeable(int x) {
+    string xStr = to_string(x);
+    for (int i=0; i<xStr.size(); ++i) if (!P[xStr[i]-'0']) return false;
+    return true;
 }
 
-// A에 경우의 수 (순열) 기록
 int solve() {
-    if (AtoNum()==n) return A.size();
-    // if (A.size() > 6) return 987654321;
-
     int ret = 987654321;
-    for (int i=0; i<12; ++i) {
-        if (P[i]) {
-            A.push_back(i);
-            ret = min(ret, solve());
-            A.pop_back();
-        }
+    //바로 +-
+    ret = min(ret, abs(n-100));
+    // 숫자 입력 후 +-
+    for (int i=0; i<1000001; ++i) {
+        if (makeable(i)) ret = min(ret, static_cast<int>(abs(n-i)+to_string(i).size()));
     }
     return ret;
 }
 
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    cin >> n; 
-    int m; cin >> m; P.resize(12);
-    for (int i=0; i<12; ++i) P[i] = true;
+    cin >> n; P.resize(10);
+    for (int i=0; i<10; ++i) P[i] = true;
+    int m; cin >> m;
     for (int i=0; i<m; ++i) {
         int x; cin >> x;
         P[x] = false;
