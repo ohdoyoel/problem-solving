3aa231b27feb47abd62e78a6766da92b88870272
Daniel Oh <ohdoyoel@gmail.com>
2024-12-20 21:54:05 +0900

    boj 1931 failed

---
+++ boj/1931/main.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int n;
+vector<pair<int, int>> M;
+vector<int> A;
+
+void solve() {
+    sort(M.begin(), M.end(), [](const auto &a, const auto &b) {
+        return a.second < b.second;
+    });
+
+    int idx = 0;
+    A.push_back(0);
+    for (int i=0; i<n; ++i) {
+        if (M[idx].second <= M[i].first) {
+            idx = i;
+            A.push_back(idx);
+        }
+    }
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> n; M.resize(n);
+    for (int i=0; i<n; ++i) {
+        int a, b; cin >> a >> b;
+        M[i] = {a, b};
+    }
+    solve();
+    cout << A.size() << endl;
+}
