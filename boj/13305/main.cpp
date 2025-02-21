39fa34efbb1a04512c6375e98f3b475377b45486
Daniel Oh <ohdoyoel@gmail.com>
2025-02-22 01:17:44 +0900

    boj 13305 main

--- boj/13305/main.cpp
+++ boj/13305/main.cpp
@@ -4,16 +4,14 @@ using namespace std;
 
 int n;
 vector<int> P, D;
-set<pair<int,int>> minPI;
 
-long long solve(int i, int j) {
-    if (i==j) return 0;
-
-    int minP=0, minPIdx=0;
-    for (auto [p, idx] : minPI) { if (i<=idx && idx<j) { minP=p; minPIdx=idx; break; } }
-
-    long long ret = solve(i, minPIdx);
-    for (int i=minPIdx; i<j; ++i) ret += (long long)minP*D[i];
+long long solve(int n) {
+    long long ret = 0;
+    long long minP = 9876543210;
+    for (int i=0; i<n; ++i) {
+        if (P[i] < minP) minP = P[i];
+        ret += minP * D[i];
+    }
     return ret;
 }
 
@@ -21,9 +19,6 @@ int main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     cin >> n; n--; P.resize(n); D.resize(n);
     for (int i=0; i<n; ++i) cin >> D[i];
-    for (int i=0; i<n; ++i) {
-        cin >> P[i];
-        minPI.insert({P[i], i});
-    }
-    cout << solve(0, n+1) << endl;
+    for (int i=0; i<n; ++i) cin >> P[i];
+    cout << solve(n) << endl;
 }
